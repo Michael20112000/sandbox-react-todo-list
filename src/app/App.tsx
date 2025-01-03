18ee95e73b3bceb528c8795426e2f954d46242ae
Michael20112000 <official.michael.developer@gmail.com>
2025-01-03 18:20:55 +0200

    checkpoint

--- src/app/App.tsx
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <div className='h-32 w-1/2 bg-amber-300'>app 42 42</div>
-}
---
+++ src/pages/about/ui/AboutPage.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router'
+
+export const AboutPage = () => {
+  return (
+    <div>
+      <Link to='/'>Go to home page</Link>
+    </div>
+  )
+}
